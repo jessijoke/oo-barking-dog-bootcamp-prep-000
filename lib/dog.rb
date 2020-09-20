bbc20971cfa4ba10825ca6f3927dae2cd80e33c7
Jessica Bradham <jessijoke@yahoo.com>
2020-09-20 02:42:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,23 @@
 # Your code goes here!
 class Dog
-    def 
+    def initialize(name, bark)
+      @name = name
+      @bark = bark
+    end
+    
+    def name(name)
+      @name = name
+    end
+    
+    def bark(bark)
+      @bark = bark
+    end
+    
+    def name
+      @name
+    end
+    
+    def bark
+      @bark
+    end
 end
