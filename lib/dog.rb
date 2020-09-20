3cd6fb9160dd6c8eb8186b985d3403fed1f21ae4
Jessica Bradham <jessijoke@yahoo.com>
2020-09-20 02:49:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,33 +1,5 @@
 # Your code goes here!
 class Dog
-    def initialize(name, bark = "woof!")
-      @name = name
-      @bark = bark
-    end
-    
-    def name(name)
-      @name = name
-    end
-    
-    def bark(bark)
-      @bark = bark
-    end
-    
-    def name
-      @name
-    end
-    
-    def bark
-      @bark
-    end
-end
-
-fido = Dog.new("Fido")
-puts fido
-  
-
-
-class Dog
   def initialize(name, bark = "woof!")
     @name = name
     @bark = bark
